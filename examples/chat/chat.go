491e3d3ff6c7ef6b1e0d45bbd0574cd62e2ef0f2
John-Alan Simmons <simmons.johnalan@gmail.com>
2014-01-20 23:43:57 -0500

    Accidentally deleted some files from last commit

---
+++ examples/chat/chat.go
@@ -0,0 +1,48 @@
+// A simple chat program leveraging websockets and the wsHub package
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"../../../wsHub"
+)
+
+var (
+	hub wsHub.WsHub
+)
+
+func main() {
+	fmt.Println("Creating WebSocket comm hub..")
+	hub = wsHub.NewHub()
+	fmt.Println("Running hub...")
+	go hub.Run()
+
+	http.HandleFunc("/ws/chat", wsHandler)
+	fmt.Println("Starting web server...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got new connection")
+	client, err := wsHub.NewClient(w, r)
+	if err != nil {
+		panic(err)
+	}
+
+	hub.RegisterClient(client)
+
+	go client.Start()
+	defer func() {
+		fmt.Println("Removing client from hub")
+		hub.UnregisterClient(client)
+	}()
+
+	for {
+		resp, err := client.Read()
+		if err != nil {
+			break
+		}
+		hub.Broadcast(resp)
+	}
+}
---
+++ wsHub.go
@@ -0,0 +1,81 @@
+package wsHub
+
+import (
+	"fmt"
+)
+
+//Central communitaion struct
+type WsHub struct {
+	// Registered connections.
+	connections map[*Client]bool
+
+	// Inbound messages from the connections.
+	broadcast chan []byte
+
+	// Register requests from the connections.
+	register chan *Client
+
+	// Unregister requests from connections.
+	unregister chan *Client
+
+	kill chan bool
+}
+
+//Create new hub
+func NewHub() WsHub {
+	h := WsHub{
+		broadcast:   make(chan []byte),
+		register:    make(chan *Client),
+		unregister:  make(chan *Client),
+		connections: make(map[*Client]bool),
+	}
+
+	return h
+}
+
+// Run the hub (most likely in its own goroutine)
+// Handles all communitaion between connected clients
+func (h *WsHub) Run() {
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = true
+		case c := <-h.unregister:
+			delete(h.connections, c)
+			close(c.send)
+		case m := <-h.broadcast:
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+					fmt.Println("Broadcasting...")
+				default:
+					delete(h.connections, c)
+					close(c.send)
+					go c.ws.Close()
+				}
+			}
+		case <-h.kill:
+			break
+		}
+	}
+}
+
+// Kill the running hub
+func (h *WsHub) Stop() {
+	h.kill <- true
+}
+
+//Register a given client object
+func (h *WsHub) RegisterClient(c *Client) {
+	h.register <- c
+}
+
+//Unregister a given client
+func (h *WsHub) UnregisterClient(c *Client) {
+	h.unregister <- c
+}
+
+//Broadcast a message to all connected clients
+func (h *WsHub) Broadcast(msg []byte) {
+	h.broadcast <- msg
+}
